6ed22dd2471986aa62ba4a54424bda9a23c9d675
DoQuocBinh <binhdq@fpt.edu.vn>
2021-09-04 15:58:01 +0700

    Add project files.

---
+++ DemoNetCoreApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DemoNetCoreApp1
+{
+    class Program
+    {
+        static void display(int n)
+        {
+            if (n == 0)
+                return;
+            else
+            {
+                Console.WriteLine(n);
+                display(n - 1);
+            }
+
+        }
+        static int sumNoneTail(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+                return n + sumNoneTail(n - 1);
+        }
+        static int sumTail(int n, int acc)
+        {
+            if (n == 0)
+                return acc;
+            else
+            {
+                return sumTail(n - 1, n + acc);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //int b = 4;
+            //Console.WriteLine(a+b);
+            //display(5);
+            //Console.WriteLine(sum(5));
+            Console.WriteLine(sumTail(5,0));
+        }
+    }
+}
+//899;
+//5 + sum(4) 
+//5+ 4 + sum(3)
+//5 + 4+ sum(2)...=> 5 + 4 + 3 + 2 + 1 + 0
